c126cb57881fe40797361bc79c393302d4ca1e4d
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-07-18 12:43:40 -0300

    Ajustes

--- js/menu-blog.js
+++ js/menu-blog.js
@@ -1,5 +1,18 @@
 const btnFechar = document.querySelector(".js-btn-fecharBlog");
+const btnDescubra = document.querySelector(".js-btn-descubraAqui");
+const scrollBarra = document.querySelector(".js-scroll-barra");
+
+btnDescubra.addEventListener("click", () => {
+   const blog = document.querySelector(".js-blog-corrida"); 
+   blog.classList.add("ativo");
+   scrollBarra.classList.add("ativo") 
+});
 
 btnFechar.addEventListener("click", () => {
-    
-})
+    const blog = document.querySelector(".js-blog-corrida"); 
+    blog.classList.remove("ativo");
+    scrollBarra.classList.remove("ativo") 
+ });
+
+
+
